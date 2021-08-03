bd4af0fc6d37562a921813f92f0fe9b00c070aa2
浅原昌大 <citron1605@gmail.com>
2021-08-03 17:57:04 +0900

    apiの接続先切り替え

--- projects/react-frontend/src/App.js
+++ projects/react-frontend/src/App.js
@@ -2,17 +2,31 @@ import logo from "./logo.svg";
 import "./App.css";
 import { FC, useEffect, useState } from "react";
 
+//サーバー接続の切り替え
+let api;
+switch (window.location.host) {
+    case process.env.REACT_APP_SERVER_DEV_HOST:
+        api = process.env.REACT_APP_SERVER_DEV_URL;
+        break;
+    case process.env.REACT_APP_SERVER_HOST:
+        api = process.env.REACT_APP_SERVER_URL;
+        break;
+    default:
+        api = ''
+}
+
 const App = () => {
     const [state, setState] = useState();
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_SERVER_URL}/hello_world`)
+        fetch(`${api}/hello_world`)
             .then((response) => response.json())
             .then((response) => setState(response))
             .catch((e) => {
                 console.log(e);
             });
     }, []);
+
     console.log(state);
 
     return (
